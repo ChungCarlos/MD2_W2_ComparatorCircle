14b9c1e2ff6bfcac80f431ccd26b6a778b0e0e7a
Chung <Joschungcarlos95@gmail.com>
2023-03-31 11:26:58 +0700

    done ComparatorCircle

---
+++ src/Circle.java
@@ -0,0 +1,44 @@
+public abstract class Circle {
+    private double radius;
+    private String color;
+
+    private boolean filled;
+
+    public Circle() {
+    }
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public Circle(double radius, String color, boolean filled) {
+        this.radius = radius;
+        this.color = color;
+        this.filled = filled;
+    }
+
+    public double getArea() {
+        return Math.PI * getRadius() * getRadius();
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return " Radius:" + getRadius() + " Area:" + getArea() + " Color:" + getColor();
+    }
+}
